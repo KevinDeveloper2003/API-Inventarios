99a0b37ff1db910124adce3d408d3f85cf322448
ElkevinM <kevin.monsalvem@est.iudigital.edu.co>
2024-06-15 19:56:41 -0500

    Update valide-admin-rol.js
    correccion de errores 3

--- middleware/valide-admin-rol.js
+++ middleware/valide-admin-rol.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const valideRoplAdmin = (req, res, next) => {
     if (req.payload.rol != 'administrador') {
-        return res.status(401).json({mesagge: 'Error Unauthorized'});
+        return res.status(401).json({message: 'Error Unauthorized'});
     }
     next();
 
